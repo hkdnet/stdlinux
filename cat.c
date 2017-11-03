3884739c960c37ef28d035363b635e81a63f88f1
hkdnet <satoko.itse@gmail.com>
2017-11-04 01:32:15 +0900

    Use stdin when no filename are specified

--- cat.c
+++ cat.c
@@ -4,23 +4,39 @@
 #include <unistd.h>
 
 static void do_cat(const char *path);
+static void do_stdin_cat();
 static void die(const char *s);
 
 int main(int argc, char const* argv[])
 {
     int i;
     if (argc < 2) {
-        fprintf(stderr, "%s: file name not given \n", argv[0]);
-        exit(1);
-    }
-    for (i = 1; i < argc; i++) {
-        do_cat(argv[i]);
+        do_stdin_cat();
+    } else {
+        for (i = 1; i < argc; i++) {
+            do_cat(argv[i]);
+        }
     }
     return 0;
 }
 
 #define BUFFER_SIZE 2048
 
+static void
+do_stdin_cat()
+{
+    unsigned char buf[BUFFER_SIZE];
+    int n;
+
+    char *s = "stdin";
+
+    for(;;) {
+        n = read(STDIN_FILENO, buf, sizeof buf);
+        if (n < 0) die(s);
+        if (n == 0) break;
+        if (write(STDOUT_FILENO, buf, n) < 0) die(s);
+    }
+}
 static void
 do_cat(const char *path)
 {
