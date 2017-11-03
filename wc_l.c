f4b8a22411cc1c8615a5e2443e343b8c2ebf4a90
hkdnet <satoko.itse@gmail.com>
2017-11-04 01:49:14 +0900

    Show file lines

--- wc_l.c
+++ wc_l.c
@@ -1,4 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+static void die(const char *s);
+static int do_wc_l(const char *path);
+#define BUFFER_SIZE 2048
+
+
 int main(int argc, char const* argv[])
 {
+    int i;
+    int count;
+    const char* path;
+    if (argc < 2) {
+        fprintf(stderr, "2args");
+        exit(1);
+    } else {
+        for (i = 1; i < argc; i++) {
+            path = argv[i];
+            count = do_wc_l(argv[i]);
+            printf("%8d %s\n", count, path);
+        }
+    }
     return 0;
+}
+
+static int
+do_wc_l(const char *path)
+{
+    int fd;
+    unsigned char buf[BUFFER_SIZE];
+    int n;
+    int i;
+    int count = 0;
+    fd = open(path, O_RDONLY);
+    if (fd < 0) die(path);
+
+    for(;;) {
+        n = read(fd, buf, sizeof buf);
+        if (n < 0) die(path);
+        if (n == 0) break;
+        for(i = 0; i < n; i++) {
+            if(buf[i] == '\n') count++;
+        }
+    }
+
+    return count;
+}
+
+static void
+die(const char *s)
+{
+    perror(s);
+    exit(1);
 }
