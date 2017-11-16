5417188c451e819904db03ee9a1d12aa3ce100b5
hkdnet <satoko.itse@gmail.com>
2017-11-17 06:17:57 +0900

    Use fgets instead of getline

--- tail.c
+++ tail.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    char** line;
-    long len;
-} line_t;
-
 static void do_tail(FILE* f);
-static void print_line_t(FILE* f, line_t l);
 
 #define DEFAULT_N_LINES 10
+#define MAX_LINE_LENGTH 1024
 
 int main(int argc, char * const argv[])
 {
@@ -22,36 +17,24 @@ static void
 do_tail(FILE* f)
 {
     int i;
-    int linecnt = 0;
-    int cur;
-    line_t buf[DEFAULT_N_LINES];
+    int cur = 0;
+    long linecnt = 0;
+    char buf[DEFAULT_N_LINES][MAX_LINE_LENGTH] ;
 
-    char* line;
-    size_t linecap;
-    ssize_t linelen;
-    while((linelen = getline(&line, &linecap, f)) >= 0) {
-        line_t tmp = { &line, linelen };
-        buf[linecnt % DEFAULT_N_LINES] = tmp;
+    while(fgets(buf[cur], MAX_LINE_LENGTH, f)) {
         linecnt++;
+        cur = linecnt % DEFAULT_N_LINES;
     }
-
-    if (!feof(f)) exit(1);
 
     cur = linecnt % DEFAULT_N_LINES;
 
     if (linecnt >= DEFAULT_N_LINES) {
         for(i = cur; i < DEFAULT_N_LINES; i++) {
-            print_line_t(stdout, buf[i]);
+            printf("%s", buf[i]);
         }
     }
     for(i = 0; i < cur; i++) {
-        print_line_t(stdout, buf[i]);
+        printf("%s", buf[i]);
     }
 
-}
-
-static void
-print_line_t(FILE* f, line_t l)
-{
-    fwrite(l.line, sizeof(char), l.len, f);
 }
