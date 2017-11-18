53c22ccc40994fbffd5208352c4b9c93095d6ac4
hkdnet <satoko.itse@gmail.com>
2017-11-19 01:50:05 +0900

    impl

--- ls.c
+++ ls.c
@@ -1,4 +1,28 @@
+#include <stdio.h>
+#include <dirent.h>
+
+typedef struct dirent dirent_t;
+static void do_dir(DIR* d);
+
 int main(int argc, char const* argv[])
 {
+    if (argc != 2) {
+        printf("Usage: %s DIR", argv[0]);
+    }
+
+    DIR* d;
+    d = opendir(argv[1]);
+    do_dir(d);
+    closedir(d);
+
     return 0;
+}
+
+static void
+do_dir(DIR* d)
+{
+    dirent_t* ent;
+    while((ent = readdir(d))) {
+        printf("%s\n", ent->d_name);
+    }
 }
