288b996fd44224f87696ac11380dddca37a2b7c8
hkdnet <satoko.itse@gmail.com>
2017-11-23 02:57:54 +0900

    Add -p option

--- mkdir.c
+++ mkdir.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 static void do_mkdir(const char* path);
 
-int main(int argc, char const* argv[])
+int main(int argc, char * const argv[])
 {
     int i;
-    if (argc < 2) {
+    int c;
+    int is_p;
+
+    while((c = getopt(argc, argv, "p")) != -1) {
+        switch (c) {
+            case 'p':
+                is_p = 1;
+                break;
+            case '?':
+                printf("Usage: %s [-p] DIR [DIR ...]", argv[0]);
+                exit(1);
+        }
+    }
+
+    if (optind == argc) {
         printf("Usage: %s DIR [DIR ...]", argv[0]);
         exit(1);
     }
 
-    for (i = 1; i < argc; i++) {
-        do_mkdir(argv[i]);
+    for (i = optind; i < argc; i++) {
+        if (is_p) do_mkdir(argv[i]);
+        else do_mkdir(argv[i]);
     }
 
     return 0;
