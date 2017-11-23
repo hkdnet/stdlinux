7266c4be84aa58acce19c81f1a92e0e920a979c5
hkdnet <satoko.itse@gmail.com>
2017-11-24 02:14:10 +0900

    ex

---
+++ open_rename_close.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char const* argv[])
+{
+    if (argc != 3) {
+        fprintf(stderr, "error\n");
+        exit(1);
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    rename(argv[1], argv[2]);
+
+
+    if (close(fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+
+    return 0;
+}
