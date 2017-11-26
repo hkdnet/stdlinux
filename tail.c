1716db11ad2b90225f176c3c1094cf10d70d8a83
hkdnet <satoko.itse@gmail.com>
2017-11-27 00:29:26 +0900

    Read files

--- tail.c
+++ tail.c
@@ -10,6 +10,7 @@ static void do_tail(FILE* f, int nlines);
 
 int main(int argc, char * const argv[])
 {
+    int i;
     int c;
     int nlines = DEFAULT_N_LINES;
     while((c = getopt(argc, argv, "n:")) != -1) {
@@ -18,11 +19,25 @@ int main(int argc, char * const argv[])
                 nlines = atoi(optarg);
                 break;
             case '?':
-                fprintf(stderr, "Usage: %s [-n NLIINES]", argv[0]);
+                fprintf(stderr, "Usage: %s [-n NLIINES] [FILE ...]", argv[0]);
                 break;
         }
     }
-    do_tail(stdin, nlines);
+
+    if (optind == argc) {
+        do_tail(stdin, nlines);
+        return 0;
+    }
+
+    for(i = optind; i < argc; i++) {
+        FILE* f = fopen(argv[i], "r");
+        if (!f) {
+            perror("open");
+            exit(1);
+        }
+        do_tail(f, nlines);
+        fclose(f);
+    }
 
     return 0;
 }
