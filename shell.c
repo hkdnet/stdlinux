44db7c8ab4e52e1bfac147023cbe59bdc7fbe8f0
hkdnet <satoko.itse@gmail.com>
2017-11-27 01:56:20 +0900

    Handle exit status

--- shell.c
+++ shell.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define MAX_LINE_LENGTH 1024
 
 int main(int argc, char const* argv[])
 {
     char buf[MAX_LINE_LENGTH];
+    int status = 0;
 
     printf("$ "); // prompt
     while(fgets(buf, sizeof(buf), stdin)) {
-        printf("%s", buf);
-        printf("$ "); // prompt
+        int pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            exit(1);
+        }
+        if (pid == 0) { // child
+            execl("/bin/cat", "cat", NULL);
+        }
+        else { // parent
+            printf("$ "); // prompt
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status)) {
+                int exitstatus = WEXITSTATUS(status);
+                if (exitstatus != 0) fprintf(stderr, "process exited with %d", exitstatus);
+                continue;
+            }
+            if (WIFSIGNALED(status)) {
+                int sig = WTERMSIG(status);
+                fprintf(stderr, "signal: %d\n", sig);
+                continue;
+            }
+            fprintf(stderr, "abnormal exit\n");
+            exit(status);
+        }
+
     }
     return 0;
 }
