68c72293a46a736e6794bba7c9362376d717b8ff
hkdnet <satoko.itse@gmail.com>
2017-12-16 15:41:01 +0900

    Revert "Merge pull request #17 from hkdnet/malloc"
    This reverts commit ab8155d23e7434e7bcdc69b3f52fe0e1cd045a38, reversing
    changes made to 580ffe265904b08dcb428cd78af2107dac856283.

--- tail.c
+++ tail.c
@@ -48,20 +48,18 @@ do_tail(FILE* f, int nlines)
     int i;
     int cur = 0;
     long linecnt = 0;
-    char* buf;
-    /*
-     * <--------------------------- ... -> malloc
-     * <-- line 1 --><-- line 2 --> ... -> usage
-     */
-    void* ptr = malloc(sizeof(char) * MAX_LINE_LENGTH * nlines);
-    if (!ptr) {
-        perror("malloc");
-        exit(1);
+    char* buf[nlines];
+    for (i = 0; i < nlines; i++) {
+        void* ptr = malloc(sizeof(char) * MAX_LINE_LENGTH);
+        if (!ptr) {
+            perror("malloc");
+            exit(1);
+        }
+        buf[i] = (char *)ptr;
+        memset(buf[i], 0, sizeof(char) * MAX_LINE_LENGTH);
     }
-    buf = (char *)ptr;
-    memset(buf, 0, sizeof(char) * MAX_LINE_LENGTH * nlines);
 
-    while(fgets(buf + (cur * MAX_LINE_LENGTH), MAX_LINE_LENGTH, f)) {
+    while(fgets(buf[cur], MAX_LINE_LENGTH, f)) {
         linecnt++;
         cur = linecnt % nlines;
     }
@@ -70,12 +68,14 @@ do_tail(FILE* f, int nlines)
 
     if (linecnt >= nlines) {
         for(i = cur; i < nlines; i++) {
-            printf("%s", buf + (i * MAX_LINE_LENGTH));
+            printf("%s", buf[i]);
         }
     }
     for(i = 0; i < cur; i++) {
-        printf("%s", buf + (i * MAX_LINE_LENGTH));
+        printf("%s", buf[i]);
     }
 
-    free(buf);
+    for (i = 0; i < nlines; i++) {
+        free(buf[i]);
+    }
 }
