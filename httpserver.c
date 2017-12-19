15bea224d9b731e6754a9557026b8e6ea80ccfeb
hkdnet <satoko.itse@gmail.com>
2017-12-20 00:35:47 +0900

    16.5

--- httpserver.c
+++ httpserver.c
@@ -5,6 +5,21 @@
 #include <signal.h>
 #include <sys/errno.h>
 
+struct HTTPHeaderField {
+    char *name;
+    char *value;
+    struct HTTPHeaderField *next;
+};
+
+struct HTTPRequest {
+    int protocol_minor_verison;
+    char *method;
+    char *path;
+    struct HTTPHeaderField *header;
+    char *body;
+    long length;
+};
+
 void
 log_exit(char *fmt, ...)
 {
@@ -47,5 +62,54 @@ void
 install_signal_handlers(void)
 {
     trap_signal(SIGPIPE, signal_exit);
+}
+
+struct HTTPRequest*
+read_request(FILE* in)
+{
+    return NULL;
+}
+
+void
+respond_to(struct HTTPRequest *req, FILE* out, const char* docroot)
+{
+}
+
+void
+free_request(struct HTTPRequest *req)
+{
+    struct HTTPHeaderField *target, *tmp;
+    target = req->header;
+    while (target) {
+        tmp = target->next;
+        free(target->name);
+        free(target->value);
+        free(target);
+        target = tmp;
+    }
+    free(req->method);
+    free(req->path);
+    free(req->body);
+    free(req);
+}
+
+void
+service(FILE* in, FILE* out, const char* docroot)
+{
+    struct HTTPRequest *req;
+    req = read_request(in);
+    respond_to(req, out, docroot);
+    free_request(req);
+}
+
+int main(int argc, char const* argv[])
+{
+    if(argc != 2) {
+        fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+        exit(1);
+    }
+    install_signal_handlers();
+    service(stdin, stdout, argv[1]);
+    return 0;
 }
 
