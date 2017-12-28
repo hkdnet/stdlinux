e0fd4a1e31c204b687495a78157421c3320606ac
hkdnet <satoko.itse@gmail.com>
2017-12-28 22:10:36 +0900

    Read file

--- httpserver.c
+++ httpserver.c
@@ -1,13 +1,21 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <time.h>
+#include <unistd.h>
 #include <sys/errno.h>
 #include <sys/stat.h>
 
 #define LINE_BUF_SIZE 1024
 #define MAX_REQUEST_BODY_LENGTH 1024
+#define TIME_BUF_SIZE 64
+#define BLOCK_BUF_SIZE 1024
+#define HTTP_MINOR_VERSION req->protocol_minor_verison
+#define SERVER_NAME "httpserver"
+#define SERVER_VERSION "0.1"
 
 struct HTTPHeaderField {
     char *name;
@@ -232,6 +240,31 @@ not_found(struct HTTPRequest *req, FILE *out)
 }
 
 void
+output_common_header_fields(struct HTTPRequest *req, FILE *out, char *status)
+{
+    time_t t;
+    struct tm *tm;
+    char buf[TIME_BUF_SIZE];
+
+    t = time(NULL);
+    tm = gmtime(&t);
+    if (!tm) log_exit("gemtime failed %s", strerror(errno));
+    strftime(buf, TIME_BUF_SIZE, "%a, %d %b %Y %H:%M:%S GMT", tm);
+    fprintf(out, "HTTP/1.%d: %s\r\n", HTTP_MINOR_VERSION, status);
+    fprintf(out, "Date: %s\r\n", buf);
+    fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
+    fprintf(out, "Connection: close\r\n");
+
+}
+
+char*
+guess_content_type(struct FileInfo *info)
+{
+    // TODO: impl...
+    return "text/html";
+}
+
+void
 do_file_response(struct HTTPRequest *req, FILE* out, const char* docroot)
 {
     struct FileInfo *info;
@@ -240,6 +273,31 @@ do_file_response(struct HTTPRequest *req, FILE* out, const char* docroot)
         free_fileinfo(info);
         not_found(req, out);
         return;
+    }
+
+    output_common_header_fields(req, out, "200 OK");
+    fprintf(out, "Content-Length: %ld\r\n", info->size);
+    fprintf(out, "Content-Type: %s\r\n", guess_content_type(info));
+    fprintf(out, "\r\n");
+    if (strcmp(req->method, "HEAD") == 0) {
+        fflush(out);
+        free_fileinfo(info);
+        return;
+    }
+    int fd;
+    char buf[BLOCK_BUF_SIZE];
+    ssize_t n;
+    fd = open(info->path, O_RDONLY);
+    if (fd < 0)
+        log_exit("failed to open %s: %s", info->path, strerror(errno));
+
+    for (;;) {
+        n = read(fd, buf, BLOCK_BUF_SIZE);
+        if (n < 0)
+            log_exit("failed to read %s: %s", info->path, strerror(errno));
+        if (n == 0) break;
+        if(fwrite(buf, n, 1, out) < n)
+            log_exit("failed to write to socket: %s", strerror(errno));
     }
 }
 
