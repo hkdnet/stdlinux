ef8fd6f2f85675b6ee602f5d9e616f58d1293afa
hkdnet <satoko.itse@gmail.com>
2017-12-31 20:59:25 +0900

    impl listen_socket

--- httpserver.c
+++ httpserver.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -9,6 +10,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/errno.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 
 #define LINE_BUF_SIZE 1024
@@ -19,6 +21,8 @@
 #define SERVER_NAME "httpserver"
 #define SERVER_VERSION "0.1"
 #define USAGE "Usage: %s [--port=n] [--chroot --user=u --group=g] <docroot>\n"
+#define MAX_BACKLOG 5
+#define DEFAULT_PORT "80"
 
 static int debug_mode = 0;
 static struct option longopts[] = {
@@ -356,8 +360,37 @@ service(FILE* in, FILE* out, const char* docroot)
 int
 listen_socket(char* port)
 {
-    // TODO: impl
-    return 0;
+    struct addrinfo hints, *res, *ai;
+    int err;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((err = getaddrinfo(NULL, port, &hints, &res)) != 0)
+        log_exit((char*)gai_strerror(err)); // cast????
+
+    for(ai = res; ai; ai = ai->ai_next) {
+        int sock;
+        sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+        if (sock < 0) continue;
+
+        if(bind(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+            close(sock);
+            continue;
+        }
+
+        if (listen(sock, MAX_BACKLOG) < 0) {
+            close(sock);
+            continue;
+        }
+        freeaddrinfo(res);
+        return sock;
+    }
+
+    log_exit("falied to listen socket");
+    return -1; // NOT REACH
 }
 
 void
